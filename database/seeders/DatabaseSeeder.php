873a55ebb905816ee0567464f1b20b82ec7aa814
Rahmat-dkh <rahmat.hdyt.3754@gmail.com>
2026-07-03 14:12:17 +0700

    chore: update admin credentials in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,31 +3,28 @@
 namespace Database\Seeders;
 
 use App\Models\Setting;
-        \App\Models\User::factory()->create([
-            'name' => 'Admin SDN Kroyolor',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('rahmat123'),
-        ]);
-
-        // Set default theme
-        Setting::create(['key' => 'theme', 'value' => 'default']);
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        \App\Models\User::truncate();
-        \App\Models\User::factory()->create([
+        User::truncate();
+        
+        User::factory()->create([
             'name' => 'Admin SDN Kroyolor',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('rahmat123'),
+            'email' => 'admin@sdnkroyolor.sch.id',
+            'password' => Hash::make('SDNKroyolor26'),
         ]);
+
+        if (Setting::where('key', 'theme')->doesntExist()) {
+            Setting::create(['key' => 'theme', 'value' => 'default']);
+        }
     }
 }
